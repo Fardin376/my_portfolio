d023c4cee5551992c54325634245121ad0dae70c
Fardin376 <ifardin72@gmail.com>
2024-04-13 20:51:36 +0600

    hero edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,29 +29,25 @@ const Hero = () => {
         </div>
       </div>
 
-      <motion.div
-        variants={slideIn('right', 'tween', 0.2, 1)}
-        className="w-full mx-auto flex items-center justify-center absolute inset-0 top-[-20px]"
-      >
+      <div className="w-full mx-auto flex items-center justify-center relative inset-0 top-[-20px]">
         <ComputerCanvas />
-      </motion.div>
-
-      <div className="absolute xs:bottom-10 md:bottom-10 bottom-4 w-full flex justify-center items-center">
-        <a href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: 'loop',
-              }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </a>
+        <div className="absolute xs:bottom-10 md:bottom-10 bottom-4 w-full flex justify-center items-center">
+          <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+              <motion.div
+                animate={{
+                  y: [0, 24, 0],
+                }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop',
+                }}
+                className="w-3 h-3 rounded-full bg-secondary mb-1"
+              />
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
